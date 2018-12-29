c6c03901a2649a17bfd299bb710b402788726feb
Emil Dimitrov <edimitrov1@gmail.com>
2018-12-30 00:12:49 +0200

    Breadth-first search

--- lib/BinaryTree.rb
+++ lib/BinaryTree.rb
@@ -8,6 +8,28 @@ class BinaryTree
     return root
   end
 
+  def self.breadth_first_search(tree, value)
+    return nil if tree.nil?
+    
+    q = []
+    q << tree
+
+    node = nil
+    while(!q.empty?)
+      if q[0].data == value
+        node = q[0]
+        break
+      end
+
+      q << q[0].left unless q[0].left.nil? 
+      q << q[0].right unless q[0].right.nil?
+      q.shift
+    end
+
+    return node
+
+  end
+
   private
   def self.insert(node, value)
     if node.nil?
--- lib/run.rb
+++ lib/run.rb
@@ -1,4 +1,6 @@
 require "./lib/Node"
 require "./lib/BinaryTree"
 
-p BinaryTree.build_tree([2, 1, 5, 8, 4])
+tree = BinaryTree.build_tree([2, 1, 5, 8, 4])
+
+p BinaryTree.breadth_first_search(tree, 5)
