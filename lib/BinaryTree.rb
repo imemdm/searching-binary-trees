c2dd9acb0b3704932004bb87633b41e540c84654
Emil Dimitrov <edimitrov1@gmail.com>
2018-12-30 23:14:47 +0200

    Depth-first search with a stack

--- lib/BinaryTree.rb
+++ lib/BinaryTree.rb
@@ -15,19 +15,40 @@ class BinaryTree
     q << tree
 
     node = nil
-    while(!q.empty?)
-      if q[0].data == value
-        node = q[0]
+    until(q.empty?)
+      if q.first.data == value
+        node = q.first
         break
       end
 
-      q << q[0].left unless q[0].left.nil? 
-      q << q[0].right unless q[0].right.nil?
+      q << q.first.left unless q.first.left.nil? 
+      q << q.first.right unless q.first.right.nil?
       q.shift
     end
 
     return node
+  end
 
+  def self.depth_first_search(tree, value)
+    return nil if tree.nil?
+
+    s = []
+    s << tree
+
+    node = nil
+    until(s.empty?)
+      if s.last.data == value
+        node = s.last
+        break
+      end
+
+      current = s.pop
+      
+      s << current.left unless current.left.nil?
+      s << current.right unless current.right.nil?
+    end
+
+    return node
   end
 
   private
--- lib/run.rb
+++ lib/run.rb
@@ -3,4 +3,4 @@ require "./lib/BinaryTree"
 
 tree = BinaryTree.build_tree([2, 1, 5, 8, 4])
 
-p BinaryTree.breadth_first_search(tree, 5)
+p BinaryTree.depth_first_search(tree, 5)
